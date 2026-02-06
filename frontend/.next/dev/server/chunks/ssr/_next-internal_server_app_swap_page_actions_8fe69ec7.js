bca5352493d901bd880249331ca8ee390692951e
Kiran <145308198+krnkiran22@users.noreply.github.com>
2026-02-06 21:33:15 +0530

    Delete frontend/.next/dev directory

--- frontend/.next/dev/server/chunks/ssr/_next-internal_server_app_swap_page_actions_8fe69ec7.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/swap/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_swap_page_actions_8fe69ec7.js.map
--- frontend/.next/dev/static/chunks/app_swap_page_tsx_31f2a789._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@mysten_sui_dist_grpc_index_mjs_62d55a4c._.js",
-  "static/chunks/_cd2f1fcf._.js",
-  "static/chunks/node_modules_34b9b17c._.js"
-],
-    source: "dynamic"
-});
